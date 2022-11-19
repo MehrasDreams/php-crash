05a3001b36d3b1cbd4671157ff20d4fb49fb9919
MehrasDreams <mehrasdeep2007@gmail.com>
2022-11-19 07:05:50 -0500

    Start the project learn about echo, print

---
+++ dreams.php
@@ -0,0 +1,53 @@
+<?php
+
+//echo 'Hello MehrasDreams'   // you should write ; in the end of the line of your code
+echo 123, 'Codeing', 10.4; // use multiple value
+
+
+print 'Linux';  // print is also print text just like python but print can't print multiple value
+
+
+
+
+
+// comment in php
+
+
+
+/*
+  Multi commenting just like java script
+
+ */
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>My first project on php</title>
+
+
+
+</head>
+
+
+<body>
+
+
+<h1><? echo 123; ?></h1 // Use php code or login in html tag just like django {%  %}
+
+
+
+</body>
+
+
+
+
+
+
+</html>
+
+
